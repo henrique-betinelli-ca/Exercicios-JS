24df5cb9518f3439adbad8232bade3f4946c20ea
henrique-betinelli-ca <henrique.betinelli@contaazul.com>
2025-08-14 18:07:58 -0300

    Exercícios

--- ex25/script.js
+++ ex25/script.js
@@ -6,25 +6,18 @@ function Calcular() {
     let n = document.getElementById('Numero')
     let numero = Number(n.value)
     n.value = ''
-    resultado = document.getElementById('ResultadoNumero')
-    
-    if(verificar(numero)){
-        resultado.innerHTML = 'Seu numero <strong>é primo</strong>'
-    } else {
-        resultado.innerHTML = 'Seu numero <strong>não é primo</strong>'
-    }
+
+    let resultado = verificarNumeroPrimo(numero) ? 'é primo' : 'não é primo'
+
+    mensagem = document.getElementById('ResultadoNumero')
+    mensagem.innerHTML = 'Seu numero <strong>' + resultado + '</strong>'
 }
 
-function verificar(numero) {
-    if (numero <= 1) {
-        return false
+function verificarNumeroPrimo(numero) {
+    if (numero <= 1) return false
+    for (let c = 2; c < numero; c++) {
+        if (numero % c == 0) return false
+    }
+    return true
 
-    } else {
-        for (let c = 2; c < numero; c++) {
-            if (numero % c == 0){
-                return false
-            }
-        }
-        return true
-    } 
 }
